83fc49991cea5e36199f97f63d26a2e4f9c91121
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-02-17 13:44:42 +0600

    server with parameterized end point

--- http-server.js
+++ http-server.js
@@ -7,11 +7,31 @@ const http = require("http");
 
 const server = http.createServer();
 
+const person = [
+  {
+    id: 0,
+    name: "Emon",
+  },
+  {
+    id: 1,
+    name: "John",
+  },
+];
+
 server.on("request", (req, res) => {
+  const url = req.url.split("/");
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Welcome home" }));
-  } else if (req.url === "/about") {
+  } else if (url[1] === "person") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    if (url.length === 3) {
+      res.end(JSON.stringify(person[Number(url[2])]));
+    } else {
+      res.end(JSON.stringify(person));
+    }
+  } else if (url[1] === "about") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.write("<h1>About Page</h1>");
