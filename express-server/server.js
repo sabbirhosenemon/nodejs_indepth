43486844c13b738463dee060062026c94a9b2b6f
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-02-18 11:43:22 +0600

    get, post using express with some validation

--- express-server/server.js
+++ express-server/server.js
@@ -10,25 +10,46 @@ const friends = [
   {
     id: 1,
     name: "John",
-  },
-  {
-    id: 2,
-    name: "Raj",
-  },
+  }
 ];
 
+// all the requests will go in through the middlewares and then process by the server and then again go through middlewares as response.
+// this logger middleware should be at the top of the stack so that as much as possible time is logged.
+app.use((req, res, next) => {
+  const start = Date.now(); // time when the request was made
+  next();
+  const last = Date.now() - start; // this is the time that server took to run the request. last time after processing the request when the response is sent.
+  console.log(`${req.method} ${req.url} - took ${last}ms`);
+});
+
+app.use(express.json()); // this is the middleware that parses the incoming request body and converts it into json.
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) { // validate the request body to make sure that the name is there.
+    return res.status(400).json({ error: "Name is required" });
+  }
+
+  const friend = {
+    id: friends.length,
+    name: req.body.name,
+  };
+  friends.push(friend);
+  res.json(friend);
+});
+
 app.get("/friends", (req, res) => {
-  res.send(friends);
+  res.send(friends); // we can also use res.json(friends)
 });
 
 app.get("/friends/:id", (req, res) => {
-    const id = Number(req.params.id)
-    if(friends[id]){ // undefined is a falsy value
-        res.status(200).json(friends[id]) // res.send(friends[id]) is also valid but we are doing specifically. by using json() -> text is converted to json
-    } else {
-        res.status(404).json({error: "id does not exist"})
-    }
-})
+  const id = Number(req.params.id);
+  if (friends[id]) {
+    // undefined is a falsy value
+    res.status(200).json(friends[id]); // res.send(friends[id]) is also valid but we are doing specifically. by using json() -> text is converted to json
+  } else {
+    res.status(404).json({ error: "id does not exist" });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
